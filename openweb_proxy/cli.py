f1e350096aeba7307a9660613d4d8abd4764cefb
jbara <medanisjbara@proton.me>
2023-07-26 13:34:28 +0100

    better file handling in case not found

--- openweb_proxy/cli.py
+++ openweb_proxy/cli.py
@@ -258,8 +258,13 @@ class ProxyMiner:
             web (bool, optional): loads from Web as fallback. Defaults to True (forced if file doesn't exist).
         """
         if not os.path.exists(filename):
-            with open(filename, "w"):
-                web = True
+            log.warning(f"File {filename} not found")
+            if web:
+                log.warning("Will load from Web")
+                self.get()
+            else:
+                log.warning("Nothing to do, please add `--web` to pull from web")
+            return
         with open(filename, "r+") as p:
             proxies = p.read().splitlines()
             if proxies:
